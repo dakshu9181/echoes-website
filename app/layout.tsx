2b7e4f0ad89a1d725630ec0377b6fa4c93a13d72
dakshu9181 <dakshu.official588@gmail.com>
2026-05-09 10:10:44 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Echoes of a Better Dawn",
+  description: "Official author website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
